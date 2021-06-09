ab7c29f651e25647c891706093502fac840fa54c
Juan David Gómez Forero <33003522+jgomezf@users.noreply.github.com>
2021-06-08 20:18:19 -0500

    add Form container

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -1,37 +1,5 @@
 import React from "react";
-
-class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("constructor Form");
-  }
-
-  componentDidMount() {
-    console.log("componentDidMount Form");
-  }
-
-  componentDidUpdate() {
-    console.log("componentDidUpdate Form");
-  }
-
-  componentWillUnmount() {
-    console.log("componentWillUnmount Form");
-    // NO Ajax
-    // No setState
-  }
-
-  render() {
-    console.log("render Form");
-    return (
-      <form onSubmit={this.props.handleSave}>
-        <input type="text" name="song" />
-        <input type="text" name="artist" />
-        <button type="submit">Save</button>
-        <button onClick={this.props.toggleForm}>Cancel</button>
-      </form>
-    );
-  }
-}
+import Form from "./Form";
 
 class FormContainer extends React.Component {
   state = {
